73b7d416ab90efee6ae75b31c65d9602225bebe5
Feitong Lu <feitonglu@gmail.com>
2020-05-08 11:36:36 -0700

    Initial Commit

---
+++ Udder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by Feitong Lu on 5/7/20.
+//  Copyright © 2020 Feitong Lu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
